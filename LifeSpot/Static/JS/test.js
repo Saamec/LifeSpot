15fbb69d85f51d486ee927b01a91bc91557ec7d1
«Ilya <333zero777@mail.ru>
2023-04-09 20:52:55 +0300

    31.4 проблемы с картинками

--- LifeSpot/Static/JS/test.js
+++ LifeSpot/Static/JS/test.js
@@ -1,3 +1,11 @@
-﻿let elements = document.getElementsByTagName('*');
+﻿const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
 
-alert(`Количество элементов на странице:  ${elements.length}`);
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
